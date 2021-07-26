8df4600883079e83635c9c20c534c18775dfa7fd
wdssmq <wdssmq@qq.com>
2021-07-26 16:08:00 +0800

    dataset 插件

---
+++ plugins/dataset.js
@@ -0,0 +1,42 @@
+import fs from 'fs-extra'
+import path from 'path'
+import yaml from 'yaml'
+
+const ROOT = path.resolve(__dirname, '..')
+
+const DATA_DIR = path.join(ROOT, 'data')
+
+const rltData = {}
+
+const files = fs.readdirSync(DATA_DIR).filter((x) => x.endsWith('.yml'))
+
+// console.log(files);
+
+for (const file of files) {
+  const content = fs.readFileSync(path.join(DATA_DIR, file)).toString()
+  const fileNoExt = file.replace('.yml', '')
+  try {
+    const parsed = yaml.parse(content)
+    rltData[fileNoExt.trim(".yml")] = parsed
+  } catch (e) {
+    console.warn(`Site config ${file} is invalid: ${e.message}`)
+  }
+}
+
+console.log(rltData);
+
+const script = `export default ${JSON.stringify(rltData)}`
+
+export default function datasetPlugin() {
+  const virtualFileId = 'virtual:dataset'
+
+  return {
+    name: 'dataset',
+    resolveId(id) {
+      if (id === virtualFileId) return virtualFileId
+    },
+    load(id) {
+      if (id === virtualFileId) return script
+    }
+  }
+}
