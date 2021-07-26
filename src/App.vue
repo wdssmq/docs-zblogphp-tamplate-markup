9a87580f11ebdb1fdf109fa9b728e14b1654ef4c
wdssmq <wdssmq@qq.com>
2021-07-26 16:19:41 +0800

    TagsTable 组件及调用；

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-
+  {{ msg }}
+  <tags-table
+    v-for="(subTags, name) in db.tags"
+    :cate="name"
+    :id="`tags-${name}`"
+    :tags="subTags"
+    :key="name"
+  ></tags-table>
 </template>
 
 <script>
+import db from "virtual:dataset";
+import TagsTable from "./components/tags-table.vue";
+
+// console.log(db.tags);
 
 export default {
-
-}
-
+  data() {
+    return {
+      msg: "App.vue",
+      db,
+    };
+  },
+  components: {
+    TagsTable,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/tags-table.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ cate }}</h1>
+    <table>
+      <tr>
+        <th>标签</th>
+        <th>含义</th>
+        <th>备注</th>
+      </tr>
+      <tr v-for="tag in tags" :key="tag">
+        <td>{{ tag.name }}</td>
+        <td>{{ tag.desc }}</td>
+        <td>{{ tag.note }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  tags: Object,
+  cate: String,
+});
+</script>
+
