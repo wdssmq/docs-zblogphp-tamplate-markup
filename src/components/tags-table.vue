4ef13c2f71a98da2e04b79d0c7eb7e7553868671
wdssmq <wdssmq@qq.com>
2021-08-02 18:51:23 +0800

    增加标签复制按钮

--- src/components/tags-table.vue
+++ src/components/tags-table.vue
@@ -4,11 +4,19 @@
     <table class="tags-table">
       <tr>
         <th>标签</th>
+        <th class="w-5">复制</th>
         <th>含义</th>
         <th>备注</th>
       </tr>
-      <tr v-for="tag in tags" :key="tag">
-        <td>{{ tag.name }}</td>
+      <tr v-for="(tag, index) in tags" :key="tag">
+        <td :class="`${cate}-${index}`">
+          {{ tag.name }}
+        </td>
+        <td>
+          <button class="btn" :data-clipboard-target="`.${cate}-${index}`">
+            复制
+          </button>
+        </td>
         <td>{{ tag.desc }}</td>
         <td>{{ tag.note }}</td>
       </tr>
@@ -20,6 +28,23 @@
 defineProps({
   tags: Object,
   cate: String,
+});
+
+import Clipboard from "clipboard";
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const clipboard = new Clipboard(".btn");
+
+clipboard.on("success", async function (e) {
+  e.trigger.innerHTML = "成功";
+  await sleep(3000);
+  e.trigger.innerHTML = "复制";
+  e.clearSelection();
+});
+
+clipboard.on("error", function (e) {
+  e.trigger.innerHTML = "复制失败";
 });
 </script>
 
@@ -38,6 +63,12 @@ defineProps({
     padding: 0.3em 0.5em;
     vertical-align: top;
     border: 1px solid #ccc;
+    &.w-5 {
+      width: 5%;
+    }
+    &.w-15 {
+      width: 15%;
+    }
   }
 }
 </style>
